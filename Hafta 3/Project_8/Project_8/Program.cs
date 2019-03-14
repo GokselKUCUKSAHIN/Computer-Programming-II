b374206f0bf183dbce9ca882bd2f3b4f9e803993
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-14 13:18:43 +0300

    Add files via upload

---
+++ Hafta 3/Project_8/Project_8/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sensor;
+            int KlimaDerecesi = 15;
+        //  do
+        //    {
+                Console.Write("Derece: ");
+                sensor = Int32.Parse(Console.ReadLine());
+                if (sensor < -10)
+                {
+                    Console.WriteLine("Hava Çok Soğuk, klima 5 derece artti");
+                    KlimaDerecesi += 5;
+                }
+                else if ((sensor >= -10) && (sensor <10))
+                {
+                    Console.WriteLine("Hava Soğuk, klima 2 derece artti");
+                    KlimaDerecesi += 2;
+                }
+                else if((sensor >= 10) && (sensor < 20))
+                {
+                    Console.WriteLine("Hava Ilık, Derecede değişiklik yapılmadı"); 
+                }
+                else
+                {
+                    Console.WriteLine("Hava Sıcak, klima 2 derece azaltıldı");
+                    KlimaDerecesi -= 2;
+                }
+            //}
+          //  while (sensor != 76);
+            switch(KlimaDerecesi)
+            {
+                case 15:
+                    {
+                        Console.WriteLine("Mavi LED yandı");
+                        break;
+                    }
+                case 18:
+                    {
+                        Console.WriteLine("Yeşil LED yandı");
+                        break;
+                    }
+                case 20:
+                    {
+                        Console.WriteLine("Kırmızı LED yandı");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Sarı LED yandı");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hafta 3/Project_9/Project_9/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //not al
+            //not 50den büyükse geçti
+            //değilse kaldi yazsin
+
+            Console.Write("Not: ");
+            int Not = Int32.Parse(Console.ReadLine());
+            if((Not >= 50) && (Not <= 100))
+            {
+                Console.WriteLine("Geçti");
+            }
+            else if((Not <50) && (Not >=0))
+            {
+                Console.WriteLine("Kaldı");
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz aralık!\n");
+            }
+            //
+            //Program 1
+            //
+
+
+            if ((Not <= 100) && (Not >= 85))
+                Console.WriteLine("AA");
+            else if ((Not < 85) && (Not >= 75))
+                Console.WriteLine("BA");
+            else if ((Not < 75) && (Not >= 65))
+                Console.WriteLine("BB");
+            else if ((Not < 65) && (Not >= 57))
+                Console.WriteLine("CB");
+            else if ((Not < 57) && (Not >= 50))
+                Console.WriteLine("CC");
+            else if ((Not > 100) || (Not < 0))
+            {
+                if (Not > 100)
+                    Console.WriteLine("100 den büyük değer giremezsiniz");
+                else
+                    Console.WriteLine(" dan küçük değer giremezsiniz");
+            }
+            else
+                Console.WriteLine("Kaldınız");
+            Console.ReadKey();
+        }
+    }
+}
