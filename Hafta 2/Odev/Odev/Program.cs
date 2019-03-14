93d23d08678774fd018a7680285aea226e5eb5e7
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-14 13:27:37 +0300

    Add files via upload

---
+++ Hafta 2/Odev/Odev/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1. Sayı: ");
+            double sayi1 = Double.Parse(Console.ReadLine());
+            Console.Write("2. Sayı: ");
+            double sayi2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("{0} Sayısı ile {1} Sayısının:\nToplamı = {2}\nFarkı = {3}\nÇarpımı = {4}\nBölümü = {5}",sayi1,sayi2,sayi1+sayi2,sayi1-sayi2,sayi1*sayi2,sayi1/sayi2);
+            Console.ReadKey();
+        }
+    }
+}
