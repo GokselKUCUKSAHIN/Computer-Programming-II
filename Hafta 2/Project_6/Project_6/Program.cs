d51aa8ea009bd5c4437faebbde1026f1451084e4
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-03-14 13:28:48 +0300

    Add files via upload

---
+++ Hafta 2/Project_6/Project_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Vize,Final;
+            Console.Write("Vize Notunuz?: ");
+            Vize = Int32.Parse(Console.ReadLine());
+            Console.Write("\nFinal Notunuz?: ");
+            Final = Int32.Parse(Console.ReadLine());
+            double ortalama = (Vize + Final) / 2.0;
+            Console.WriteLine("Vize Notunuz: {0}, Final Notunuz: {1}, Ortalamanız = {2}", Vize, Final, ortalama);
+
+
+
+            Console.ReadKey();
+          
+        }
+    }
+}
